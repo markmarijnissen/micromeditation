45c3a0ff0c020aa38546dd1fd6624e64a3172302
Mark Marijnissen <markmarijnissen@gmail.com>
2023-05-18 01:28:21 +0200

    try to add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false
+    ssr: false,
+    modules: [
+        // '@vite-pwa/nuxt'
+    ],
+    pwa: {
+        registerType: 'autoUpdate',
+        manifest: {
+            name: 'MicroMeditation',
+            short_name: 'µMeditation',
+            theme_color: '#000000',
+            icons: [
+                {
+                    src: 'icon-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+            ],
+        },
+        // workbox: {
+        //     navigateFallback: '/',
+        //     globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+        // },
+        // client: {
+        //     installPrompt: true,
+        // },
+        devOptions: {
+            // enabled: true,
+            type: 'module',
+        },
+    }
 })
